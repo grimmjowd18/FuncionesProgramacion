f43f1f927e2fed5f4616baf7ccfbd2df86d132aa
User <User@192.168.0.5>
2014-07-04 00:57:05 -0500

    Creación Clase Ejercicio7

--- FuncionesProgramacion/src/funcionesprogramacion/Ejercicio7.java
+++ FuncionesProgramacion/src/funcionesprogramacion/Ejercicio7.java
@@ -6,10 +6,36 @@
 
 package funcionesprogramacion;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author User
  */
 public class Ejercicio7 {
-    
+    public static void main(String[] args) 
+    {        
+        int codigo;
+        char x;
+        String a="";
+        String Cadena1, aux;
+        Cadena1=JOptionPane.showInputDialog("Ingrese Frase");
+        int longitud1 = Cadena1.length();    
+        char [] frase1= Cadena1.toCharArray();    
+        for (int i = 0; i < longitud1; i++)
+        {
+            if (Cadena1.codePointAt(i)>=65 && Cadena1.codePointAt(i)<=90)
+            {
+                codigo = Cadena1.codePointAt(i);
+                codigo =codigo+32;
+                x=(char)codigo; 
+                a=a+x;                    
+            }
+            else                  
+            {
+                a=a+frase1[i];                    
+            }          
+        }
+        JOptionPane.showMessageDialog(null,a);
+    }
 }
